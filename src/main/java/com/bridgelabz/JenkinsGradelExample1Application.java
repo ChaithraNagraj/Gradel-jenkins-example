7adf4f7726fa97026bc327d5421ad1a32462abf7
chaithra-harshi <chaithrabn96@gmail.com>
2020-05-30 09:07:41 +0530

    adding

---
+++ src/main/java/com/bridgelabz/JenkinsGradelExample1Application.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGradelExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGradelExample1Application.class, args);
+		System.out.println("Here is the frst jenkins pipeline example ");
+
+	}
+
+}
